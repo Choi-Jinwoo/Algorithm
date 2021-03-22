378a7de5c78af7fd530a1d444991d46d4779307d
Choi-jinwoo <chlwlsdn0828@gmail.com>
2021-03-22 11:35:29 +0900

    Add: 1차 캐시

--- 프로그래머스/가장 큰 수/main.py
+++ 프로그래머스/가장 큰 수/main.py
@@ -1,15 +1,6 @@
 def solution(numbers):
-    str_numbers = list(map(str, numbers))
-
-    str_numbers.sort(reverse=True)
-
-    for j in range(len(str_numbers)):
-        for i in range(len(str_numbers) - 1):
-            if str_numbers[i + 1] == str_numbers[i][:-1] and str_numbers[i][-1] < str_numbers[i + 1][-1]:
-                str_numbers[i], str_numbers[i + 1] = str_numbers[i + 1], str_numbers[i]
-
-    print(str_numbers)
-    print(''.join(str_numbers))
+    numbers.sort()
+    print(numbers)
 
 
-solution([3, 302, 34, 5, 9, 30])
+solution(['03', '3'])
--- 프로그래머스/신규 아이디 추천/main.py
+++ 프로그래머스/신규 아이디 추천/main.py
@@ -30,26 +30,29 @@ def merge_period(id: str):
 
 
 def solution(new_id: str):
-    new_id_len = len(new_id)
-
+    # 1단계
     new_id = new_id.lower()
+
+    # 2단계
     new_id = delete_exclude_char(new_id)
+
+    # 3,4단계
     new_id = merge_period(new_id)
 
+    # 5단계
     if len(new_id) <= 0:
         new_id = 'a'
 
+    # 6단계
     if len(new_id) >= 16:
         new_id = new_id[0:15]
 
     if new_id[-1] == '.':
         new_id = new_id[:-1]
 
+    # 7단계
     if len(new_id) <= 2:
         while len(new_id) <= 2:
             new_id = new_id + new_id[-1]
 
     return new_id
-
-
-print(solution('aaaaaaaaaabbbb.c'))
