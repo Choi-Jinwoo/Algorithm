854af0ec89b3cc92cfbdf7798a0d9bb2d44d750e
Choi-Jinwoo <chlwlsdn0828@gmail.com>
2021-11-08 10:14:11 +0900

    add: 숫자 문자열과 영단어

---
+++ 프로그래머스/1단계/숫자 문자열과 영단어/index.js
@@ -0,0 +1,20 @@
+const mapWordToNum = {
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+};
+
+function solution(s) {
+  for (const word in mapWordToNum) {
+    s = s.replace(new RegExp(word, "g"), mapWordToNum[word]);
+  }
+
+  return Number(s);
+}
